27ac7458453edfa9394a044983a27351091f4edb
TheSunSkys <thesunskys@gmail.com>
2024-12-24 11:15:57 +0700

    fea: add font

---
+++ fonts/THSarabunNew.js
@@ -0,0 +1,8 @@
+module.exports = {
+	Roboto: {
+		normal: __dirname + '/THSarabunNew/THSarabunNew.ttf',
+		bold: __dirname + '/THSarabunNew/THSarabunNew Bold.ttf',
+		italics: __dirname + '/THSarabunNew/THSarabunNew Italic.ttf',
+		bolditalics: __dirname + '/THSarabunNew/THSarabunNew BoldItalic.ttf'
+	}
+};
